9219ef0cb5f58fdeebc5ae738bf36db34ce66888
German <thewhitehaven04@gmail.com>
2023-08-12 23:13:28 +0200

    theme switching wip

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import { setupCarouselAnimation } from './carousel';
 import style from './styles/style.css';
-import {
-  getToggleIconButton,
-  prefersDarkSchemeQuery,
-  toggleThemeSwitcherButtonOnPreferSchemeChange,
-} from './themeSwitcher';
+import { ThemeTogglerService, ThemePersistenceService } from './themeSwitcher';
 import '/src/themeSwitcher';
 
 /** @type NodeListOf<HTMLDivElement> */
@@ -12,10 +8,9 @@ const carousels = document.querySelectorAll('.carousel');
 setupCarouselAnimation(carousels[0]);
 
 export const themeToggleButton = document.querySelector('#theme-toggle');
-themeToggleButton?.appendChild(
-  getToggleIconButton(prefersDarkSchemeQuery.matches),
-);
 
-prefersDarkSchemeQuery.addEventListener('change', (e) =>
-  toggleThemeSwitcherButtonOnPreferSchemeChange(e, themeToggleButton),
+const themeTogglerService = new ThemeTogglerService(
+  themeToggleButton,
+  new ThemePersistenceService(),
 );
+themeTogglerService.applyPreferredThemeIfDefined();
--- src/themeSwitcher.js
+++ src/themeSwitcher.js
@@ -1,28 +1,100 @@
 import { icon } from '@fortawesome/fontawesome-svg-core';
 
-export const prefersDarkSchemeQuery = window.matchMedia(
-  '(prefers-color-scheme: dark)',
-);
-/**
- * @param {Boolean} matchesDark
- */
-export const getToggleIconButton = (matchesDark) => {
-  const placeholderNode = document.createElement('div');
+const PREFERRED_THEME_KEY = 'preferredTheme';
+const DARK_THEME_CLASS_NAME = 'dark-theme';
+
+const getIconForTheme = (/** @type {boolean} */ isDarkTheme) => {
   let buttonIcon;
-  if (matchesDark) {
+  if (isDarkTheme) {
     buttonIcon = icon({ iconName: 'sun', prefix: 'fas' });
   } else {
     buttonIcon = icon({ iconName: 'moon', prefix: 'fas' });
   }
-
-  placeholderNode.appendChild(buttonIcon.node[0]);
-  return placeholderNode;
+  return buttonIcon.node[0];
 };
 
-/**
- * @param {MediaQueryListEvent} e
- * @param {HTMLButtonElement} button
- */
-export const toggleThemeSwitcherButtonOnPreferSchemeChange = (e, button) => {
-  button?.replaceChildren(getToggleIconButton(e.matches));
-};
+export class ThemePersistenceService {
+  getPreferredThemeFromLocalStorage() {
+    return localStorage.getItem(PREFERRED_THEME_KEY);
+  }
+
+  savePreferredThemeToLocalStorage(theme) {
+    localStorage.setItem(PREFERRED_THEME_KEY, theme);
+  }
+}
+export class ThemeTogglerService {
+  /**
+   * @param {?Element} button
+   * @param {ThemePersistenceService} themePersistenceService
+   */
+  constructor(button, themePersistenceService) {
+    this.themePersistenceService = themePersistenceService;
+
+    this.toggleButton = button;
+    this.bodyElement = document.querySelector('body');
+
+    this.prefersDarkSchemeQuery = window.matchMedia(
+      '(prefers-color-scheme: dark)',
+    );
+
+    this.isOverridden = Boolean(
+      themePersistenceService.getPreferredThemeFromLocalStorage(),
+    );
+
+    this.prefersDarkSchemeQuery.addEventListener('change', (e) => {
+      const isDarkTheme = e.matches;
+      if (!this.isOverridden) {
+        this.#setTheme(isDarkTheme);
+        this.#setButtonTheme(isDarkTheme);
+      }
+    });
+
+    this.toggleButton?.addEventListener('click', this.#toggleOnClick);
+  }
+
+  #toggleOnClick(e) {
+    const currentUserPreferredTheme =
+      this.themePersistenceService.getPreferredThemeFromLocalStorage();
+    const theme =
+      currentUserPreferredTheme ?? this.prefersDarkSchemeQuery
+        ? 'dark'
+        : 'light';
+    const isDarkTheme = theme === 'dark';
+    this.#setPreferredTheme(!isDarkTheme);
+  }
+
+  /** @param {boolean} isDarkTheme */
+  #setButtonTheme(isDarkTheme) {
+    this.button?.replaceChildren(getIconForTheme(isDarkTheme));
+  }
+
+  /** @param {boolean} isDarkTheme */
+  #setTheme(isDarkTheme) {
+    if (isDarkTheme) {
+      this.bodyElement?.classList.add(DARK_THEME_CLASS_NAME);
+    } else {
+      this.bodyElement?.classList.remove(DARK_THEME_CLASS_NAME);
+    }
+  }
+
+  /** @param {boolean} isDarkTheme */
+  #setPreferredTheme(isDarkTheme) {
+    this.#setTheme(isDarkTheme);
+    this.#setButtonTheme(isDarkTheme);
+
+    const preferredTheme = isDarkTheme ? 'dark' : 'light';
+    this.themePersistenceService.savePreferredThemeToLocalStorage(
+      preferredTheme,
+    );
+  }
+
+  applyPreferredThemeIfDefined() {
+    const isDefined =
+      this.themePersistenceService.getPreferredThemeFromLocalStorage();
+    if (isDefined) {
+      const isDarkTheme = isDefined === 'dark';
+      this.#setButtonTheme(isDarkTheme);
+      this.#setTheme(isDarkTheme);
+    }
+  }
+}
