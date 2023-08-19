de9de963fdf03c13aa98a143577a5afbcede442f
German <thewhitehaven04@gmail.com>
2023-08-19 18:03:50 +0200

    responsive header

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import { setupCarouselAnimation } from './carousel';
-import style from './styles/style.css';
-import { ThemeTogglerService, ThemePersistenceService } from './themeSwitcher';
-import '/src/themeSwitcher';
+import { setupCarouselAnimation } from "./carousel";
+import style from "./styles/style.css";
+import flex from "./styles/flex.css";
+import positioning from "./styles/positioning.css";
+import box from "./styles/box.css";
+import { ThemeTogglerService, ThemePersistenceService } from "./themeSwitcher";
+import "/src/themeSwitcher";
 
 /** @type NodeListOf<HTMLDivElement> */
-const carousels = document.querySelectorAll('.carousel');
+const carousels = document.querySelectorAll(".carousel");
 carousels.forEach((carousel) => setupCarouselAnimation(carousel));
 
-const themeToggleButton = document.querySelector('#theme-toggle');
-const body = document.querySelector('body');
+const themeToggleButton = document.querySelector("#theme-toggle");
+const body = document.querySelector("body");
 
 const themeTogglerService = new ThemeTogglerService(
   themeToggleButton,
   body,
-  new ThemePersistenceService(),
+  new ThemePersistenceService()
 );
 themeTogglerService.applyUserPreferredThemeIfDefined();
